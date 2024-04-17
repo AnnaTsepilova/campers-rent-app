054d7f41cf5045eeb7e16167072f459a5b060ffa
Anna Tsepilova <virgo.tsav@gmail.com>
2024-04-17 21:10:35 +0300

    add svg sprite

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,41 @@
+import css from './SearchBar.module.css';
+import svgSprite from '../../img/icons.svg';
+
+import { SearchButton } from 'components/Button/Button';
+
+const SearchBar = () => {
+  return (
+    <>
+      <form
+        className={css.searchBar_wrapper}
+        //   onSubmit={onHandleSubmit}
+      >
+        <div className={css.location_wrapper}>
+          <label className={css.label} htmlFor="location">
+            Location
+          </label>
+          <div className={css.locationInput_wrapper}>
+            <input
+              className={css.location_input}
+              type="text"
+              name="location"
+              autoComplete="off"
+              placeholder="City"
+            />
+            <svg className={css.location_svg} width={18} height={20}>
+              <use href={`${svgSprite}#map-pin`}></use>
+            </svg>
+          </div>
+        </div>
+        <div className={css.filters_wrapper}>
+          <label className={css.label} htmlFor="">
+            Filters
+          </label>
+        </div>
+        <SearchButton type="submit">Search</SearchButton>
+      </form>
+    </>
+  );
+};
+
+export default SearchBar;
